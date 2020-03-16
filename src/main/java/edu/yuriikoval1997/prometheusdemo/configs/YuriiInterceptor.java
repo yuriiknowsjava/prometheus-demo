0567836c5edb626f0ef2d309833e968d70d22d1f
yuriiknowsjava <yuriiknowsjava@gmail.com>
2020-03-16 15:43:48 +0200

    Changed counter name

--- src/main/java/edu/yuriikoval1997/prometheusdemo/configs/YuriiInterceptor.java
+++ src/main/java/edu/yuriikoval1997/prometheusdemo/configs/YuriiInterceptor.java
@@ -5,37 +5,19 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class YuriiInterceptor extends HandlerInterceptorAdapter {
 
-    private final Map<String, Counter> counters = new ConcurrentHashMap<>();
+    private static final Counter counter = Counter.build()
+            .name("TOTAL_NUMBER_OF_CALLS")
+            .help("Count total number of calls")
+            .labelNames("path", "method", "status_code")
+            .create()
+            .register();
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
-        getCounter(request.getRequestURI())
-                .labels(request.getMethod(), Integer.toString(response.getStatus()))
+        counter.labels(request.getRequestURI(), request.getMethod(), Integer.toString(response.getStatus()))
                 .inc();
-    }
-
-    private Counter getCounter(String name) {
-        name = name.replaceAll("/", "_");
-        Counter counter = counters.get(name);
-        if (counter == null) {
-            synchronized (counters) {
-                counter = counters.get(name);
-                if (counter == null) {
-                    counter = Counter.build()
-                            .name(name)
-                            .help("Count total number of calls")
-                            .labelNames("method", "status_code")
-                            .create()
-                            .register();
-                    counters.put(name, counter);
-                }
-            }
-        }
-        return counter;
     }
 }
